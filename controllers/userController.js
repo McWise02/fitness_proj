cf21fc02ba4fb5055606cd44d05a8d9f2fb95f3e
McWise02 <micah02.brown@gmail.com>
2025-10-15 14:21:08 +0200

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -162,12 +162,13 @@ exports.getUserById = async (req, res) => {
 
 exports.updateMeByGithubId = async (req, res) => {
   try {
+    console.log("updateMeByGithubId called with body:", req.body);
+    console.log('Session data:', req.session);
     const githubId = req.session?.githubId;
     if (!githubId) {
       return res.status(401).json({ error: 'Not authenticated (missing githubId in session).' });
     }
 
-    // Only allow these fields to be updated from the client
     const {
       firstName,
       lastName,
